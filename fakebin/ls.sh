53d3cff51cc4085d7971f4df236b346562419d21
Tim Adye <Tim.Adye@cern.ch>
2018-07-14 13:51:34 +0100

    remove ls.sh in favour of fake ls.exe

--- fakebin/ls.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-arg=("$@")
-for ((i=0; i<${#arg[*]}; i++)); do
-  case "${arg[$i]}" in
-    [A-Z]:[/\\]*|*\\*) arg[$i]=$(/usr/bin/cygpath "${arg[$i]}");;
-  esac
-done
-set -x
-exec /usr/bin/ls "${arg[@]}"
